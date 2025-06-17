d512e94755fe1a6380ec7036152b71c613c18dec
Abdallah Abdoulhakim <aabdoulhakim@gmail.com>
2025-06-17 11:42:11 +0300

    new changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,8 +31,8 @@ app.use("/expenses", expenseRoutes);
 
 /* SERVER */
 
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT || 3001);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
